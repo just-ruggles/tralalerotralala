4a7460353cc277766cd2e266c78d6af858e6b243
just-ruggles <ammoncadal@eafit.edu.co>
2025-04-22 21:42:25 -0500

    traductor.py

--- traductor.py
+++ traductor.py
@@ -55,7 +55,7 @@ stt_button.js_on_event("button_click", CustomJS(code="""
 result = streamlit_bokeh_events(
     stt_button,
     events="GET_TEXT",
-    key="listen",
+    key="listen_button_2",
     refresh_on_update=False,
     override_height=75,
     debounce_time=0)
